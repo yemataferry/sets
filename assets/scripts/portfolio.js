28cf1903bfeb7ac4c94271277309bdccc716e967
Yemata Ferry <yemataferry@gmail.com>
2023-12-05 06:48:41 +0100

    second attempt

---
+++ assets/scripts/portfolio.js
@@ -0,0 +1,26 @@
+let box = document.getElementsByTagName("body");
+
+const band = document.getElementById("heading");
+heading.style.backgroundColor = "gray";
+heading.style.textAlignment = "center";
+// heading.style.animation = 'marquee';
+heading.style.display = "flex";
+heading.style.flexDirection = "column";
+
+// const line = document.getElementsById ('heading')
+
+
+function changeColor() {
+    const randomColor = getColor();
+    document.body.style.backgroundColor = randomColor;
+  }
+  
+  function getColor() {
+    const change = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += change[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+  setInterval(changeColor, 1000);
