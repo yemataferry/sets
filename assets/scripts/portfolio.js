1c21fc6a7de209059d95361cb144cedd28feaa4b
Yemata Ferry <yemataferry@gmail.com>
2023-12-05 07:13:30 +0100

    second attempt

--- assets/scripts/portfolio.js
+++ assets/scripts/portfolio.js
@@ -1,27 +1,22 @@
-let box = document.getElementsByTagName("body");
+let box = document.getElementsByTagName('body')
 
-const band = document.getElementById("heading");
-heading.style.backgroundColor = "gray";
-heading.style.textAlignment = "center";
-// heading.style.animation = 'marquee';
-heading.style.display = "flex";
-heading.style.flexDirection = "column";
-
-// const line = document.getElementsById ('options')
-// options.style.display = "flex";
-// options.style.flexDirection = "row";
+const band = document.getElementById('heading')
+heading.style.backgroundColor = 'gray'
+heading.style.textAlignment = 'center'
+heading.style.display = 'flex'
+heading.style.flexDirection = 'column'
 
 function changeColor() {
-    const randomColor = getColor();
-    document.body.style.backgroundColor = randomColor;
+  const randomColor = getColor()
+  document.body.style.backgroundColor = randomColor
+}
+
+function getColor() {
+  const change = "0123456789ABCDEF"
+  let color = '#'
+  for (let i = 0; i < 6; i++) {
+    color += change[Math.floor(Math.random() * 16)]
   }
-  
-  function getColor() {
-    const change = "0123456789ABCDEF";
-    let color = "#";
-    for (let i = 0; i < 6; i++) {
-      color += change[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
-  setInterval(changeColor, 1000);
+  return color
+}
+setInterval(changeColor, 1000)
